a5a397f1f8ad64cf637d1de9cd099f7188b90cf2
Welkani <baptiti54@gmail.com>
2024-02-08 14:33:59 +0100

    Calculatricev1

---
+++ Calculatrice/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Entrer votre opération : ");
+
+int nombre1 = Convert.ToInt32(Console.ReadLine());
+string operateur = Console.ReadLine();
+int nombre2 = Convert.ToInt32(Console.ReadLine());   
+
+int operation;
+
+if (string.Equals(operateur,"+")){
+    operation = (nombre1 + nombre2);    
+}
+else if(string.Equals(operateur,"-")){
+    operation = (nombre1 - nombre2);
+
+}
+else if(string.Equals(operateur,"*")){
+    operation = (nombre1 * nombre2);
+
+}
+else if(string.Equals(operateur,"/")) {
+    operation = (nombre1 / nombre2) ;
+
+}
+Console.WriteLine($"Résultat : {operation}");
+
