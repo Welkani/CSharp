84a7ce14eb49e391409ba99ff05d105b0e4d25b6
Welkani <baptiti54@gmail.com>
2024-02-08 14:00:09 +0100

    Exercice nombre

---
+++ NombreMystere/Program.cs
@@ -0,0 +1,14 @@
+﻿
+const int min = 1;
+const int max = 100;
+
+Console.Write("Quel est votre nom ? ");
+string nom = Console.ReadLine();
+Console.WriteLine($"Bienvenue {nom} ");
+
+Console.Write($"Donner un nombre entre {min} et {max} : ");
+int nombre= Convert.ToInt32(Console.ReadLine());
+Console.Write($"Le nombre est : {nombre} ");
+
+
+
